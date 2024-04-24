44d261b07de3ea9304f2f5008a0b6d9ef870fffd
nikolasbaele <nikolasbaele@gmail.com>
2024-04-24 15:22:24 +0200

    la file d'attente devient une variable globale

--- form.ts
+++ form.ts
@@ -2,42 +2,59 @@
 
 import { closeForm } from "./src/main.ts";
 
-console.log('Script started successfully');
+console.log("Script started successfully");
 
 const saveButton = document.getElementById("inscrire") as HTMLButtonElement;
 
 const firstName = document.getElementById("firstName") as HTMLInputElement;
 const lastName = document.getElementById("lastName") as HTMLInputElement;
-const username = document.getElementById("username") as HTMLInputElement;
 const phone = document.getElementById("phone") as HTMLInputElement;
 const email = document.getElementById("email") as HTMLInputElement;
-const password = document.getElementById("password") as HTMLInputElement;
+const age = document.getElementById("age") as HTMLInputElement;
+const gender = document.getElementById("gender") as HTMLInputElement;
+const searching = document.getElementById("searching") as HTMLInputElement;
 
-
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
+WA.onInit()
+  .then(() => {
+    console.log("Scripting API ready");
+    console.log(WA.state.players);
 
     saveButton.addEventListener("click", (e) => {
+      if (firstName.value === "" || lastName.value === "" || email.value === "") {
+        alert("Veuillez remplir les champs obligatoires");
+      } else {
+        alert("Inscription réussie");
 
-        if (firstName.value === "" || lastName.value === "" || email.value === "" || password.value === "") {
-            alert("Veuillez remplir les champs obligatoires");
-        } else {
-            alert("Inscription réussie");
-            
-            WA.player.state.id = Date.now() + Math.random();
-            WA.player.state.status = true;
-            WA.player.state.firstName = firstName.value;
-            WA.player.state.lastName = lastName.value;
-            WA.player.state.username = username.value;
-            WA.player.state.phone = phone.value;
-            WA.player.state.email = email.value;
-            WA.player.state.password = password.value;
-            alert(WA.player.state.id + " " + WA.player.state.firstName + " " + WA.player.state.lastName + " " + WA.player.state.username + " " + WA.player.state.phone + " " + WA.player.state.email + " " + WA.player.state.password);
-            WA.controls.restorePlayerControls();
-            e.preventDefault();
-        }
+        WA.player.state.id = Date.now() + Math.random();
+        WA.player.state.status = true;
+        WA.player.state.firstName = firstName.value;
+        WA.player.state.lastName = lastName.value;
+        WA.player.state.phone = phone.value;
+        WA.player.state.email = email.value;
+        WA.player.state.age = age.value;
+        WA.player.state.gender = gender.value;
+        WA.player.state.searching = searching.value;
+        WA.player.state.id = WA.state.loadVariable("indexPlayers");
+
+        WA.state.saveVariable("players", {
+          ...WA.state.loadVariable("players"),
+          [WA.state.loadVariable("indexPlayers")]: {
+            firstName: WA.player.state.firstName,
+            lastName: WA.player.state.lastName,
+            age: WA.player.state.age,
+            gender: WA.player.state.gender,
+            searching: WA.player.state.searching,
+            email: WA.player.state.email,
+            phone: WA.player.state.phone,
+          },
+        });
+        WA.state.saveVariable("indexPlayers", WA.state.loadVariable("indexPlayers") + 1);
+        WA.controls.restorePlayerControls();
+        e.preventDefault();
+        console.log(WA.state.players);
+      }
     });
-
-}).catch(e => console.error(e));
+  })
+  .catch((e) => console.error(e));
 
 export {};
